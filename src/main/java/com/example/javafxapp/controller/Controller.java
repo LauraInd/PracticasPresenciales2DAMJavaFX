45723c45c5cbdeb42657918cc5b3f687a4cb3fd2
JMGella <a28024@svalero.com>
2025-01-31 18:09:25 +0100

    estructura

---
+++ src/main/java/com/example/javafxapp/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.example.javafxapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+
+    @FXML
+    private TextArea txArea;
+
+
+
+
+
+}
